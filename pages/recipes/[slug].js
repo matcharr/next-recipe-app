4e62879eb83c428ca7a5c3b487759cab86cefacb
matcharr <matcharr@student.42.fr>
2021-09-20 16:15:17 +0000

    first commit

---
+++ pages/recipes/[slug].js
@@ -0,0 +1,11 @@
+import { sanityClient, urlFor, usePreviewSubscription, PortableText, } from "../../lib/sanity";
+
+const recipeQuery = ``;
+
+export default function OneRecipe() {
+
+}
+
+export default function getStaticPaths() {
+  
+}
